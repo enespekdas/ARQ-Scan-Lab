b31a587bd6d5a858349a2f5d038502b25ace794a
Enes Pekdas <38957716+enespekdas@users.noreply.github.com>
2026-04-26 23:36:15 +0300

    Update InsecureCrypto.java

--- java/src/main/java/com/arq/lab/InsecureCrypto.java
+++ java/src/main/java/com/arq/lab/InsecureCrypto.java
@@ -8,7 +8,7 @@ import java.util.Base64;
 public class InsecureCrypto {
 
     // Weak: hardcoded key + AES/ECB + MD5
-    private static final String HARDCODED_KEY = "0123456789ABCDEFA31a";
+    private static final String HARDCODED_KEY = "";
 
     public static String md5Hex(String input) throws Exception {
         MessageDigest md = MessageDigest.getInstance("MD5"); // weak
